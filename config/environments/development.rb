1e11acd3c8a394673bbb9950f6993348ebb7ae8d
k018c1072 <k018c1072@it-neec.jp>
2021-05-30 21:19:11 +0900

    Implement the devise gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,18 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # mailer setting
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "aporo0323choco@gmail.com",
+    :password => "Chiro0845",
+    :authentication => 'login'
+  }
 end
