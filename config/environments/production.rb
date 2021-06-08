2e0ce1eec3a74db55b656237f668dd5ef9c5b32c
k018c1072 <k018c1072@it-neec.jp>
2021-06-08 15:54:51 +0900

    mailer setting update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,13 +118,13 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   # mailer setting
-  config.action_mailer.default_url_options = { host: 'https://portfolio-summary.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :port => ENV['MAILGUN_SMTP_PORT'],
     :address => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name => ENV['MAILGUN_SMTP_LOGIN'],
+    user_name => ENV['MAILGUN_SMTP_LOGIN'],
     :password => ENV['MAILGUN_SMTP_PASSWORD'],
     :domain => host,
     :authentication => :plain,
