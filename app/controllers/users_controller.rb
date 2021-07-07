7a6715651b6ca194c3ed5d37a8eb48d16d546f93
k018c1072 <k018c1072@it-neec.jp>
2021-07-08 01:07:28 +0900

    マイページのcssを改良

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @portfolios = @user.portfolios.paginate(page: params[:page])
+    @portfolios = @user.portfolios.paginate(page: params[:page], per_page: 5)
   end
 
   def index
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,6 +3,7 @@ class Portfolio < ApplicationRecord
     default_scope -> { order(created_at: :desc) }
     validates :title, presence: true, length: {maximum:50}
     validates :description, presence: true, length: {maximum:255}
-    validates :url, presence: true
+    VALID_URL_REGEX = /\A#{URI::regexp(%w(http https))}\z/
+    validates :url, presence: true, format: { with: VALID_URL_REGEX }
     validates :user_id, presence: true
 end
